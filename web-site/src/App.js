48423bcb3d652c514ce666f8dc07d45c064d24c6
Tim Borisenko <Tborisen>
2020-10-06 21:52:34 -0700

    page inception

---
+++ web-site/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://tinyurl.com/y2yssnh5" className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
