844b8cf21304d6d5899146231e48b5abaca8d3b6
Tim Borisenko <Tborisen>
2020-10-08 23:21:44 -0700

    version 1.3

--- web-site/src/Components/HandleImage.js
+++ web-site/src/Components/HandleImage.js
@@ -6,25 +6,53 @@ export class HandleImage extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {imageFile: null};
+        this.state = {
+            imageFile: null,
+            hiddenText: "",
+            encrypted: false
+        };
       }
 
-    toggleUploadFile = event => {
+    toggleUploadFile = (event) => {
         console.log(event.target.files[0]);
         this.setState({imageFile: URL.createObjectURL(event.target.files[0]) });
     }
-    getText = event => {
-        console.log(event.target.value);
+
+    getText = (event) => {
+        // console.log(event.target.value);
+        this.setState({hiddenText: event.target.value});
+    }
+
+    hideTextSubmission = () => {
+        // perform action to hide text into image here
+        if (this.state.hiddenText != null && this.state.hiddenText !== "" && this.state.imageFile != null) {
+            // perform action to hide text into image here
+
+            this.setState({encrypted: true})
+            console.log(this.state.hiddenText);
+        }
+        
+    }
+    
+    ecnryptedImage = () => {
+        return (
+            this.state.encrypted ?
+            <div><br></br><h2>Ecnrypted Image:</h2><br></br><img className="imageFormat" src={this.state.imageFile}/><br></br></div>
+            : null
+        );
     }
     render () {
         return (<div>
-            <h1> Upload Image Here</h1>
-            {/* <Button variant="primary" onClick={this.toggleUploadFile}>Upload</Button>{' '} */}
-            <img className="imageFormat" src={this.state.imageFile}/> <br></br>
-            <span><input clasName="inputBtn" type="file" name="img" id="" accept="image/*" onChange={this.toggleUploadFile}/></span> 
-            <div><input className="inputText" type="text" onChange={this.getText}></input>
-            <Button variant="primary">Hide Message into Image</Button>{' '}
-            </div>
+            <h2> Upload Image Here:</h2>
+            <img className="imageFormat" src={this.state.imageFile}/><br></br>
+            <input clasName="inputBtn" type="file" name="img" id="" accept="image/*" onChange={this.toggleUploadFile}/><br></br>
+            {(this.state.imageFile != null) ? 
+                <div><input className="inputText" type="text" onChange={this.getText}></input>
+                <Button variant="primary" onClick={this.hideTextSubmission}>Hide Message into Image</Button>{' '}
+                </div> 
+            : null
+            }
+            {this.ecnryptedImage()}
             </div>);
     }
 }
