e8b3d4af1b0e136d7b425b178602643798d1ee0b
Tim Borisenko <Tborisen>
2020-11-13 21:48:47 -0800

    algorithm part1

--- web-site/src/App.js
+++ web-site/src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Button from 'react-bootstrap/Button';
 import {HandleImage} from './Components/HandleImage.js'
 
 function App() {
--- web-site/src/Components/HandleImage.js
+++ web-site/src/Components/HandleImage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import '../App.css';
+import '../steganography.min.js'
 
 export class HandleImage extends React.Component {
 
@@ -14,6 +15,8 @@ export class HandleImage extends React.Component {
             decrypted: false
         };
         this.baseState = this.state;
+       // console.log(steg); // not working
+
       }
 
     toggleUploadFile = (event) => {
@@ -35,10 +38,51 @@ export class HandleImage extends React.Component {
         // perform action to hide text into image here
         if (this.state.hiddenText != null && this.state.hiddenText !== "" && this.state.imageFile != null) {
             // perform action to hide text into image here
+           // this.performEncryption();
 
             this.setState({encrypted: true})
             console.log(this.state.hiddenText);
         }
+        
+    }
+
+    performEncryption = () => {
+        var getBit = function(number, location) {
+            return ((number >> location) & 1);
+          };
+          
+          var getBitsFromNumber = function(number) {
+            var bits = [];
+            for (var i = 0; i < 16; i++) {
+              bits.push(getBit(number, i));
+            }
+            return bits;
+          };
+
+        //   var messageBits = [];
+        // for (var i = 0; i < message.length; i++) {
+        //     var code = message.charCodeAt(i);
+        //     var bits = getBitsFromNumber(code);
+        //     messageBits = messageBits.concat(bits);
+        // }
+        
+        // var imgData = ctx.getImageData(0, 0, width, height);
+        // var colors = imgData.data;
+        // var hash = sjcl.hash.sha256.hash(password);
+        // var messageSize = 0, pos = 0;
+        // var setBit = function(number, location, bit) {
+        //     return (number & ~(1 << location)) | (bit << location);
+        //   };
+        //   // ...
+        //   colors[loc] = setBit(colors[loc], 0, messageBits[pos]);
+        // while (pos < 16) {
+        //     var rand = hash[pos % hash.length] * (pos + 1);
+        //     var loc = Math.abs(rand) % colors.length;
+        //     // ...
+        //     var bit = getBit(bytes[loc], 0);
+        //     messageSize = setBit(messageSize, pos, bit);
+        //     pos++;
+        // }
         
     }
     
@@ -80,7 +124,7 @@ export class HandleImage extends React.Component {
             <p> Upload Image To Get Started:</p> : null }
             <img className="imageFormat" src={this.state.imageFile}/>
             {(this.state.imageFile === null) ? 
-            <input clasName="inputBtn" type="file" name="img" id="" accept="image/*" onChange={this.toggleUploadFile}/>
+            <input className="inputBtn" type="file" name="img" id="" accept="image/*" onChange={this.toggleUploadFile}/>
             : null }
             {this.state.encrypted ? this.ecnryptedImage() : this.decodeAndHideTextBtns()}
             {(this.state.imageFile != null) ? <Button variant="secondary" onClick={this.reset}>Restart</Button> : null }
