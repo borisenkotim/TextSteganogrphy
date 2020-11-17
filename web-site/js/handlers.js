9e610ed1ab1d23e247bc757083d6db155302e3ca
Tim Borisenko <Tborisen>
2020-11-16 22:39:55 -0800

    added UI download btn

--- web-site/js/handlers.js
+++ web-site/js/handlers.js
@@ -15,16 +15,20 @@ fileUploaded = (input) => {
       Hide Text Into Image
      </button><br></br>`
     );
+    // reset the next image steps
+    document.querySelector('#encImageBlock').innerHTML = ``;
   } else console.log("no image loaded");
 
   reader.readAsDataURL(input.files[0]);
 }
 
 hideSecretText = () => {
+  // TODO: Optional, if have time. Add onClick event for download button that would download the encoded image.
   let done = steg.encode(document.querySelector('#secretText').value, imgUrl);
-  document.querySelector('#encImageText').innerHTML = (
+  document.querySelector('#encImageBlock').innerHTML = (
   `<h5>Encoded Image</h5>
-  <img id="encImage" width=500 src=${done} alt="" /><br></br>`
+  <img id="encImage" width=400 src=${done} alt="" /><br>
+  <button class="btn-secondary">Download</button><br></br>`
   );
 }
 
@@ -32,9 +36,12 @@ decodeImage = (input) => {
     if (input.files != null && input.files[0] != null) {
       var reader = new FileReader();
       reader.onload = function(e) {
-        let result = steg.decode(e.target.result);
-        console.log(result);
-        document.querySelector('#decodedTextDes').innerHTML = (`Decoded Text: <i>${result}</i>`);
+        try {
+          let result = steg.decode(e.target.result);
+          document.querySelector('#decodedTextDes').innerHTML = (`Decoded Text: <i>${result}</i>`);
+        } catch (e) {
+          document.querySelector('#decodedTextDes').innerText = (`No Text Found!`);
+        }
       };
     }
     reader.readAsDataURL(input.files[0]);
