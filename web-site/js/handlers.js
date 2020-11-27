38da0f6ce99a8cd171df142d4499b19283793d6e
Tim Borisenko <Tborisen>
2020-11-27 14:00:56 -0800

    algorithm update

--- web-site/js/handlers.js
+++ web-site/js/handlers.js
@@ -1,5 +1,9 @@
 
+
 var imgUrl;
+var dataUrl;
+var imageTemp;
+var canvas;
 
 fileUploaded = (input) => {
   if (input.files != null && input.files[0] != null) {
@@ -18,33 +22,99 @@ fileUploaded = (input) => {
     // reset the next image steps
     document.querySelector('#encImageBlock').innerHTML = ``;
   } else console.log("no image loaded");
-
+  
   reader.readAsDataURL(input.files[0]);
 }
 
 hideSecretText = () => {
   // TODO: Optional, if have time. Add onClick event for download button that would download the encoded image.
-  let done = steg.encode(document.querySelector('#secretText').value, imgUrl);
+  let messageText = document.querySelector('#secretText').value;
   document.querySelector('#encImageBlock').innerHTML = (
   `<h5>Encoded Image</h5>
-  <img id="encImage" width=400 src=${done} alt="" /><br>
-  <button class="btn-secondary">Download</button><br></br>`
+  <canvas id="encImage"></canvas>
+  <div id="textBlock"><canvas id="hiddenText"></canvas></div><br>`
   );
+  canvas = document.getElementById("encImage");
+  var ctx = canvas.getContext('2d');
+  var textCanvas = document.getElementById('hiddenText');
+  var tctx = textCanvas.getContext('2d');
+  var img = new Image();
+  img.onload = function(){
+    canvas.width = img.width;
+    canvas.height = img.height;
+    tctx.font = "30px Arial";
+    tctx.fillText(messageText,10,50);
+    ctx.drawImage(img,0,0);
+    var imgData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    var textData = tctx.getImageData(0, 0, canvas.width, canvas.height);
+    var pixelsInMsg = 0;
+        pixelsOutMsg = 0;
+    for (var i = 0; i < textData.data.length; i += 4) {
+        if (textData.data[i+3] !== 0) {
+            if (imgData.data[i+1]%10 == 7) {
+                //do nothing, we're good
+            }
+            else if (imgData.data[i+1] > 247) {
+                imgData.data[i+1] = 247;
+            }
+            else {
+                while (imgData.data[i+1] % 10 != 7) {
+                    imgData.data[i+1]++;
+                }
+            }
+            pixelsInMsg++;
+        }
+        else {
+            if (imgData.data[i+1]%10 == 7) {
+                imgData.data[i+1]--;
+            }
+            pixelsOutMsg++;
+        }
+    }
+    ctx.putImageData(imgData, 0, 0);
+  };
+  document.querySelector('#textBlock').innerHTML = `<button id="downloadBtn" class="btn-secondary" onclick="downloadImage()" >Download</button><br>`;
+  img.src = imgUrl;
 }
 
 decodeImage = (input) => {
+  var decodeCanvas = document.getElementById('decodeImg');
+  var dctx = decodeCanvas.getContext('2d');
     if (input.files != null && input.files[0] != null) {
       var reader = new FileReader();
-      reader.onload = function(e) {
-        try {
-          let result = steg.decode(e.target.result);
-          document.querySelector('#decodedTextDes').innerHTML = (`Decoded Text: <i>${result}</i>`);
-        } catch (e) {
+      reader.onload = function(event) {
+      try {
+          var img2 = new Image();
+          img2.onload = function(){
+              decodeCanvas.width = img2.width;
+              decodeCanvas.height = img2.height;
+              dctx.drawImage(img2,0,0);
+              var decodeData = dctx.getImageData(0, 0, decodeCanvas.width, decodeCanvas.height);
+              for (var i = 0; i < decodeData.data.length; i += 4) {
+                  if (decodeData.data[i+1] % 10 == 7) {
+                      decodeData.data[i] = 0;
+                      decodeData.data[i+1] = 0;
+                      decodeData.data[i+2] = 0;
+                      decodeData.data[i+3] = 255;
+                  }
+                  else {
+                      decodeData.data[i+3] = 0;
+                  }
+              }
+              dctx.putImageData(decodeData, 0, 0);
+          };
+          document.querySelector('#decodedTextDes').innerHTML = (`Decoded Text:`);
+          img2.src = event.target.result;
+        } catch (event) {
           document.querySelector('#decodedTextDes').innerText = (`No Text Found!`);
         }
       };
     }
-    reader.readAsDataURL(input.files[0]);
-  }
+  reader.readAsDataURL(input.files[0]);
+}
 
-
+downloadImage = () => {
+  canvas.toBlob(function(blob) {
+    saveAs(blob, "encodedImage.png");
+});
+}
